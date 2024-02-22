11d0e06f55a819530552c448be8f82188d7ab344
vgbao1231 <vgbao1231@gmail.com>
2024-02-22 23:57:37 +0700

    Create login page and admin page by Vogiabao

---
+++ src/main/java/com/CNPM/QLNT/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.CNPM.QLNT.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+    @RequestMapping("login")
+    public String login(){
+        return "login";
+    }
+    @RequestMapping("/admin")
+    public String admin(){
+        return "admin/admin";
+    }
+}
